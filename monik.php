e9d356056e10d691c48f071275f9430f3dfc17cd
Christina Monica Nauly <80596806+ChristinaMonicaNauly@users.noreply.github.com>
2022-03-02 11:18:27 +0700

    Add files via upload

---
+++ monik.php
@@ -0,0 +1,22 @@
+<?php
+
+   #for($i=1; $i<=10; $i++) {
+  #  print("Urutan ke-$i\n");
+  #}
+
+  $nim = "20090021";
+  $nama = "Christina Monica Nauly";
+  $kelas = "4C";
+  echo "
+  Nama  : $nama <br/>
+  NIM   : $nim <br/>
+  Kelas : $kelas <br/>";
+
+  #$output = shell_exec("ls -lart");
+  #echo $output;
+
+  #shell_exec("touch index.php");
+  #shell_exec("echo '<h1>Hello World</h1>' > index.php");
+  
+  
+?>
